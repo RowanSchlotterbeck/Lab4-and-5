3f2f51bc6706821b75b3568903dd3decc77006da
RowanSchlotterbeck <rdschlott@gmail.com>
2025-04-21 21:09:08 -0500

    Working State

---
+++ src/Lab4Server.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.net.*;
+import java.lang.Runnable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class Lab4Server {
+
+
+    private static int clientCounter = 0; // Used to count how many clients are connected and for stylistic purposes
+    private static final int PORT = 7225;
+
+    // Hashmap to check connected clients, useful for broadcast function
+    private static ConcurrentHashMap <String, ClientHandler> clients = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws IOException {
+
+
+        // Init server and listen on port for client connections
+        try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Listening on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // Connect Client to Server
+                System.out.println("Accepted connection from " + clientSocket.getInetAddress());
+                clientCounter++; // Increment Client Counter
+                String clientName = "Client " + clientCounter;
+
+                // Pass Client socket and the Client name to the clientHandler
+                ClientHandler clientHandler = new ClientHandler(clientSocket, clientName);
+
+
+                // Create and start a new thread for each client that joins the server
+                clients.put(clientName, clientHandler);
+                Thread clientThread = new Thread(clientHandler);
+                clientThread.start();
+
+
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+
+    }
+
+    // Broadcasts a message to all connected clients
+    private static void broadcast(String message, ClientHandler sender) {
+        for(Map.Entry<String, ClientHandler> entry : clients.entrySet() ) {
+            entry.getValue().sendMessage(message);
+            System.out.println("Sent to " + sender + ": " + message);
+        }
+
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+        private final String clientName;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        // Constructor that init variables and connect in and out
+        public ClientHandler(Socket clientSocket, String clientName) {
+
+            System.out.println("Client " + clientName + " connected");
+
+            this.clientSocket = clientSocket;
+            this.clientName = clientName;
+
+            try {
+                in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+
+                out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+            }
+            catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+
+        // Overrides the run method to recieve input from the client and broadcast it to all connected clients
+        @Override
+        public void run() {
+
+            try {
+                String receivedLine;
+
+                while ((receivedLine = in.readLine()) != null) {
+                    System.out.println("Received from " + clientName +  ": " + receivedLine);
+                    broadcast(clientName + ": " + receivedLine, this); // sends message and the ClientHandler object
+
+                }
+            }
+            catch (IOException e) {
+                System.err.println(e);
+            }
+            finally {
+                try {
+                    clientSocket.close(); // Close the client socket
+                    clients.remove(clientName); // Remove client from the clients HashMap
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+
+        public void sendMessage(String message) {
+            out.println(message); // Sends a message to the client
+        }
+    }
+}
+
+// Notes
+// Server program will need to be multithreaded in order to support multiple clients at one time
+// When a client connects, we will need to create a new thread
+
+// Important to isolate each client thread connection to prevent the race condition problem
+
+// Sequence
+// Create a server socket to listen on a specified port
+    // Connect a client to the serversocket to create two-way communication
+    // Spawn a thread to handle the client, call an instance of the client handler
+// Continue the previous two as the server continues to listen
+// Ensure that when a client closes a connection that all resources are closed
+
+
+
---
+++ src/Lab5Client.java
@@ -0,0 +1,150 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+
+
+public class Lab5Client {
+
+
+    static Socket socket = null;
+    static PrintWriter out = null;
+    static BufferedReader in = null;
+    static JTextArea textArea;
+
+    public static void main(String[] args) {
+
+        // Creates main frame
+        JFrame frame = new JFrame("Lab 5 Client");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Creates the main text area
+        textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        textArea.setEditable(false);
+
+        // Create Parent Panel
+        JPanel parentPanel = new JPanel();
+        parentPanel.setLayout(new BoxLayout(parentPanel, BoxLayout.Y_AXIS));
+
+        // Add the Message panel
+        JPanel messagePanel = new JPanel();
+        messagePanel.setLayout(new FlowLayout());
+        JButton sendButton = new JButton("Send");
+        sendButton.setEnabled(false);
+        JTextArea messageGui = new JTextArea(null, 1, 30);
+        messageGui.setEditable(false);
+
+        messagePanel.add(messageGui);
+        messagePanel.add(sendButton);
+
+
+        // Add the Connection panel
+        JPanel connectionPanel = new JPanel();
+        messagePanel.setLayout(new FlowLayout());
+
+        JLabel serverLabel = new JLabel("Server: ");
+        JTextArea serverField = new JTextArea("localhost", 1, 16);
+
+        JLabel portLabel = new JLabel("Port: ");
+        JTextArea portField = new JTextArea("7225",1, 5);
+
+        JButton connectButton = new JButton("Connect");
+
+        // Add components to Panel
+        connectionPanel.add(serverLabel);
+        connectionPanel.add(serverField);
+        connectionPanel.add(portLabel);
+        connectionPanel.add(portField);
+        connectionPanel.add(connectButton);
+
+        // Add child panels to parent panel
+        parentPanel.add(messagePanel);
+        parentPanel.add(connectionPanel);
+
+        // Add the parent panel to the frame
+        frame.add(parentPanel, BorderLayout.SOUTH);
+
+        // Sets the frame to visible
+        frame.setVisible(true);
+
+        // Get the port and address from the user
+        final int PORT = Integer.parseInt(portField.getText()); // DEFAULT = "7225"
+        final String ADDRESS = serverField.getText(); // DEFAULT = "localhost"
+
+        // Connect to the server given an address and a port number
+        connectButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                // Connect to the server
+                try {
+                    socket = new Socket(ADDRESS, PORT);
+
+
+                    out = new PrintWriter(socket.getOutputStream(), true); // Init 'out'
+
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Init 'in'
+
+
+                    System.out.println("Connected to " + socket.getInetAddress().getHostName());
+                }
+                catch (UnknownHostException UHe) {
+                    System.err.println("Could not connect to " + ADDRESS + ":" + PORT);
+                }
+                catch (IOException IOe) {
+                    System.err.println("Server not responding at " + ADDRESS + ":" + PORT);
+                }
+
+
+                // Create a thread that will listen to the server for broadcasted messages
+                Thread listenThread = new Thread(() -> {
+                    String serverMessage;
+                    try {
+                        while ((serverMessage = in.readLine()) != null) {
+                            textArea.append(serverMessage + "\n");
+                            System.out.println("Server: " + serverMessage);
+                        }
+                    } catch (IOException IOe) {
+                        System.err.println("Error receiving from server: " + IOe.getMessage());
+                    }
+                });
+
+                listenThread.setDaemon(true); // Marks this thread as a daemon thread
+                listenThread.start();
+
+
+                sendButton.setEnabled(true); // Send button gets enabled
+                messageGui.setEditable(true); // Message text field becomes editable
+                portField.setEditable(false); // Port Text field becomes UNeditable
+                serverField.setEditable(false); // Server Text field becomes UNeditable
+                connectButton.setEnabled(false); // Connect button is disabled
+
+
+
+            }
+
+        });
+
+
+        sendButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                // Sends the text to the server so that it may be broadcast
+                String message = messageGui.getText();
+                if(!message.isEmpty()) {
+                    out.println(message);
+                    messageGui.setText("");
+
+                }
+
+
+            }
+        });
+
+    }
+
+}
